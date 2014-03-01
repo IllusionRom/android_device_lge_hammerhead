a9d4c3f733ad43885e6d3cd7b22a0f799d9941f0
MightyBear007 <kiransurath9995@gmail.com>
2014-03-01 12:30:42 +0530

    New Bootanimation size

--- ill.mk
+++ ill.mk
@@ -53,4 +53,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # bootanimation
 PRODUCT_COPY_FILES += \
-        vendor/illusion/bootanimation/Illusion800x800.zip:system/media/bootanimation.zip
+        vendor/illusion/bootanimation/Illusion1080x1080.zip:system/media/bootanimation.zip
